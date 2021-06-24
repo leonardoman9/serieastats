57b22c5c88947228eafcfbae39b9e5b5bcc7b806
leonardoman9 <s1090620@studenti.univpm.it>
2021-06-24 22:19:02 +0200

    getter setters

--- src/main/java/it/univpm/demoSpringBootActivation/model/JsonParser.java
+++ src/main/java/it/univpm/demoSpringBootActivation/model/JsonParser.java
@@ -1,7 +1,15 @@
 package it.univpm.demoSpringBootActivation.model;
 
+import org.json.JSONObject;
+
 public class JsonParser {
-	public static void parser(String json) {
-		JSONObject obj = new JSONObject(json)
+	public static League leagueParser(String json) {
+		JSONObject obj = new JSONObject(json);
+		
+		
+		League serieA = new League();
+		return serieA;
+		
+	
 	}
 }
---
+++ src/main/java/it/univpm/demoSpringBootActivation/model/League.java
@@ -0,0 +1,11 @@
+package it.univpm.demoSpringBootActivation.model;
+
+public class League {
+	private int numSquadre;
+	private String areaName;
+	private String leagueName;
+	private String startDate;
+	private String endDate;
+	private Team[] teams;
+	
+}
