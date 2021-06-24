04da6382426b193fbe0bc016477888e22967f82d
leonardoman9 <s1090620@studenti.univpm.it>
2021-06-24 17:37:57 +0200

    Nuovo progetto

---
+++ src/main/java/it/univpm/demoSpringBootActivation/DemoSpringBootActivationApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.demoSpringBootActivation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootActivationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootActivationApplication.class, args);
+	}
+
+}
