699fd53da234c72722b3f480bee8b0e0f24649e8
leonardoman9 <46076388+leonardoman9@users.noreply.github.com>
2021-07-01 20:41:04 +0200

    Update Venues.java

--- src/main/java/it/univpm/demoSpringBootActivation/model/Venues.java
+++ src/main/java/it/univpm/demoSpringBootActivation/model/Venues.java
@@ -4,7 +4,6 @@ import java.util.HashSet;
 
 @SuppressWarnings({ "hiding", "serial" })
 public class Venues<String> extends HashSet<String>{
-	private String[] venues;
 	
 	public int numberOfVenues() {
 		return this.size();
@@ -13,7 +12,6 @@ public class Venues<String> extends HashSet<String>{
 		java.lang.String result="";
 		for (String i : this) {
 			result+=(i+"\n");
-			
 		}
 		return "There are "+ numberOfVenues() +" different venues: "+ "\n" + result;
 	}
